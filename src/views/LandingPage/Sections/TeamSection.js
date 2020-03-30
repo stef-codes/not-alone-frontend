99871ab6e76026e3aaa6a67105899269deb6574d
Stefanie <stefcodes@gmail.com>
2020-03-29 20:09:05 -0700

    remove unused images from team section

--- src/views/LandingPage/Sections/TeamSection.js
+++ src/views/LandingPage/Sections/TeamSection.js
@@ -17,8 +17,6 @@ import CardFooter from "components/Card/CardFooter.js";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/teamStyle.js";
 
 import team1 from "assets/img/faces/stef.jpg";
-import team2 from "assets/img/faces/christian.jpg";
-import team3 from "assets/img/faces/kendall.jpg";
 
 const useStyles = makeStyles(styles);
 
