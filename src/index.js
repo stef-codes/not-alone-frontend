0c0af02f7637832be0c173e2d504b47449e5f926
Stefanie <stefcodes@gmail.com>
2020-03-29 18:20:23 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import "assets/scss/material-kit-react.scss?v=1.8.0";
+
+// pages for this product
+import Components from "views/Components/Components.js";
+import LandingPage from "views/LandingPage/LandingPage.js";
+import ProfilePage from "views/ProfilePage/ProfilePage.js";
+import LoginPage from "views/LoginPage/LoginPage.js";
+
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route exact path="/landing-page" component={LandingPage} />
+      <Route exact path="/profile-page" component={ProfilePage} />
+      <Route exact path="/login-page" component={LoginPage} />
+      <Route exact path="/components" component={Components} />
+      <Route exact path="/" component={LandingPage} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
---
+++ src/views/LandingPage/LandingPage.js
@@ -0,0 +1,84 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPage.js";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.js";
+import TeamSection from "./Sections/TeamSection.js";
+import WorkSection from "./Sections/WorkSection.js";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function LandingPage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="Not Alone"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+        {...rest}
+      />
+      <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Connect.</h1>
+              <h4>
+                We are in unprecendented times, which cause for us to rally together.
+                There is strength in numbers. We are social distancing. But, you are not alone. 
+                Click the button to connect with someone new during this time. Share your story, 
+                without judgement and be met with empathy by another person face-to-face {"("}video chat{")"}.
+                <br/> <br/>
+                You are not alone. 
+              </h4>
+              <br />
+              <Button
+                color="rose"
+                size="lg"
+                href="https://calendly.com/wearenotealone/connect"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {/* <i className="fas fa-play" /> */}
+                 Let's Talk
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <ProductSection />
+          <TeamSection />
+          <WorkSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
